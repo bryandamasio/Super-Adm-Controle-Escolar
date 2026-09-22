cad019361a9f5d156497363e88bf271bf40a64c2
Kauan Pires <kauanpires1023-create@users.noreply.github.com>
2026-09-22 20:50:49 -0300

    Corrige rotas e acesso ao painel de professores

--- server.js
+++ server.js
@@ -26,6 +26,16 @@ app.use(cors({
 app.use(express.json({ limit: '1mb' }));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Rotas amigáveis para os painéis
+app.get('/professor', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'professor.html'));
+});
+
+app.get('/professores', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'professores.html'));
+});
+
 app.use('/qrcodes', express.static(path.join(__dirname, 'python', 'qrcodes')));
 
 app.get('/api/health', async (req, res) => {
